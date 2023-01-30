44f91d2323434ec36f5c8c40248f0c11c52c0d2a
Cesar Noriega <c79.noriega@gmail.com>
2023-01-29 23:42:21 -0600

    Added a Mock Library, a Producer, and Serialization Config.

---
+++ src/main/java/com/example/springkafkasample2/producer/Producer.java
@@ -0,0 +1,30 @@
+package com.example.springkafkasample2.producer;
+
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@Component
+public class Producer {
+
+    private final KafkaTemplate<Integer, Integer> kafkaTemplate;
+    Faker faker;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void generate(){
+        faker = Faker.instance();
+        final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+        final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+
+        Flux.zip(interval, quotes);
+    }
+
+}
