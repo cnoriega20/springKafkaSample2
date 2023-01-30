d2ad2f925498835f8317de972c705715b0237bfc
Cesar Noriega <c79.noriega@gmail.com>
2023-01-29 20:19:34 -0600

    Initial commit: Project structure.

---
+++ src/main/java/com/example/springkafkasample2/SpringKafkaSample2Application.java
@@ -0,0 +1,13 @@
+package com.example.springkafkasample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaSample2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkasample2/SpringKafkaSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkasample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaSample2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
